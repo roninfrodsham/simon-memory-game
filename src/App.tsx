18999fe584e741dbfaa0c3649faaa1a008046f1b
roninfrodsham <roninfrodsham@gmail.com>
2024-03-04 08:44:14 +0000

    move sounds into seperate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect, useReducer } from "react";
+import { useState, useEffect, useReducer, useCallback } from "react";
 import { gameReducer } from "./reducer/gameReducer";
 import { GameButton, StartButton } from "./components";
 import { getRandomInt, timeout } from "./utils/misc";
+import { soundMap, errorSound } from "./utils/sounds";
 import { Colour, ActionTypes, GameStateProps } from "./types";
 import {
   COLOURS_COUNT,
@@ -10,32 +11,13 @@ import {
   MIN_SPEED,
   AUDIO_PLAYBACK,
   TIMEOUT_DURATION,
-  LIGHT_UP_DELAY,
+  LIGHT_START_DELAY,
+  LIGHT_END_DELAY,
   ERROR_DELAY,
+  COLOURS,
 } from "./constants";
 import logo from "./logo.svg";
 import "./App.css";
-
-import greenMp3 from "./sounds/green.mp3";
-import redMp3 from "./sounds/red.mp3";
-import yellowMp3 from "./sounds/yellow.mp3";
-import blueMp3 from "./sounds/blue.mp3";
-import errorMp3 from "./sounds/error.mp3";
-
-const greenSound = new Audio(greenMp3);
-const redSound = new Audio(redMp3);
-const yellowSound = new Audio(yellowMp3);
-const blueSound = new Audio(blueMp3);
-const errorSound = new Audio(errorMp3);
-
-const soundMap = {
-  [Colour.GREEN]: greenSound,
-  [Colour.RED]: redSound,
-  [Colour.YELLOW]: yellowSound,
-  [Colour.BLUE]: blueSound,
-};
-
-const colours: Colour[] = [Colour.GREEN, Colour.RED, Colour.YELLOW, Colour.BLUE];
 
 const initialGameState: GameStateProps = {
   isGameActive: false,
@@ -51,13 +33,13 @@ function App() {
   const [gameState, dispatch] = useReducer(gameReducer, initialGameState);
   const [currentColour, setCurrentColour] = useState<string | null>(null);
 
-  const startGame = () => {
+  const startGame = useCallback(() => {
     dispatch({ type: ActionTypes.START_GAME });
-  };
+  }, []);
 
   useEffect(() => {
     if (gameState.simonMode) {
-      dispatch({ type: ActionTypes.ADD_COLOUR, payload: colours[getRandomInt(COLOURS_COUNT)] });
+      dispatch({ type: ActionTypes.ADD_COLOUR, payload: COLOURS[getRandomInt(COLOURS_COUNT)] });
     }
   }, [gameState.simonMode]);
 
@@ -68,13 +50,13 @@ function App() {
   }, [gameState.isGameActive, gameState.colours]);
 
   async function lightUpColours() {
-    await timeout(LIGHT_UP_DELAY);
+    await timeout(LIGHT_START_DELAY);
     for (let index = 0; index < gameState.colours.length; index++) {
       setCurrentColour(gameState.colours[index]);
       playSound(gameState.colours[index]);
       await timeout(gameState.speed);
       setCurrentColour("");
-      await timeout(100);
+      await timeout(LIGHT_END_DELAY);
     }
     const newGameSpeed = gameState.speed - SPEED_DECREMENT < MIN_SPEED ? MIN_SPEED : gameState.speed - SPEED_DECREMENT;
     dispatch({ type: ActionTypes.END_SIMON_MODE, payload: newGameSpeed });
@@ -109,20 +91,22 @@ function App() {
     const sound = soundMap[selectedColour];
     if (sound) {
       sound.playbackRate = gameState.audioPlayback;
-      sound.play();
+      sound.play().catch((error) => {
+        console.error(`Failed to play sound: ${error}`);
+      });
     }
   };
 
   return (
     <div className='simon circle oh bGray'>
       <div className='buttonsContainer pa circle oh'>
-        {colours.map((colour) => (
+        {COLOURS.map((colour) => (
           <GameButton
             colour={colour}
             lightUp={currentColour === colour}
             onClick={gameButtonClickHandle}
             key={colour}
-            active={gameState.isGameActive}
+            active={gameState.isGameActive && !gameState.simonMode}
           />
         ))}
         <div className='divider pa hoz bGray shadow10'></div>
--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,15 @@
-export const COLOURS_COUNT = 4;
+import { Colour } from "./types";
+
 export const INITIAL_SPEED = 800;
 export const SPEED_DECREMENT = 100;
 export const MIN_SPEED = 200;
+
 export const AUDIO_PLAYBACK = 2;
+
 export const TIMEOUT_DURATION = 200;
-export const LIGHT_UP_DELAY = 1000;
+export const LIGHT_START_DELAY = 1000;
+export const LIGHT_END_DELAY = 100;
 export const ERROR_DELAY = 500;
+
+export const COLOURS: Colour[] = [Colour.GREEN, Colour.RED, Colour.YELLOW, Colour.BLUE];
+export const COLOURS_COUNT = COLOURS.length;
